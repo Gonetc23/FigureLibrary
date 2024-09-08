d03ed127f77170b2e2afbb05c440298e2f9d317a
Gonetc23 <amadey10@bk.ru>
2024-09-08 15:16:51 +0300

    Fixing the build 0.2

---
+++ UnitTest/CircleTests.cs
@@ -0,0 +1,26 @@
+﻿using FigureLibrary;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class CircleTests
+    {
+
+        [TestMethod]
+        [DataRow("Круг", 5, 78.5)]
+        [DataRow("Круг", 10, 314)]
+        [DataRow("Круг", 15, 706.5)]
+        public void CalculateSquare_CorrectRadius_ReturnsValue(string typeName, double radius, double result)
+        {
+            //Arrange
+            var circle = new Circle(typeName, radius);
+
+            //Act
+            var act = circle.CalculateSquare();
+
+            //Assert
+            Assert.AreEqual(result, act);
+        }
+        //некорректные данные в конструктор
+    }
+}
