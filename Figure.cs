beff95866ed02396713f987ad1eff5a6907637b8
Gonetc23 <amadey10@bk.ru>
2024-09-08 15:05:39 +0300

    Fixing the build

---
+++ Figure.cs
@@ -0,0 +1,20 @@
+﻿namespace FigureLibrary
+{
+    public abstract class Figure
+    {
+        public string TypeName { get; set; }
+
+        public Figure(string typeName)
+        {
+            TypeName = typeName;
+        }
+
+        public abstract double CalculateSquare();
+
+        public override string ToString()
+        {
+            return $"Текущий тип фигуры: " + TypeName;
+        }
+    }
+}
+
